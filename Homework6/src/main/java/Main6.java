1fdac0b0fa10eb5a51085314d2af4a68a29661cc
saul-1395 <zel-1395@yandex.ru>
2019-03-06 16:34:51 +0300

    Start

--- Homework6/src/main/java/Main6.java
+++ Homework6/src/main/java/Main6.java
@@ -3,7 +3,7 @@ import java.util.Random;
 
 public class Main6 {
 
-    private static Tree.TraverseMode IN_ORDER ;
+
 
     public static void main(String[] args) {
         Tree<Integer> tree = new TreeImpl<>();
@@ -13,39 +13,8 @@ public class Main6 {
 
        addTreeCycle(treeList, 20, 30);
 
-
-       //вот тут взял из листа одно дерево, и ничего не произошло
-        for (int i = 0; i < treeList.size(); i++) {
-            System.out.println(TreeImpl.isBalanced(( treeList.get(1)).getRootNode()) );
-        }
-
-
-
-
-
-        tree.add(60);
-        tree.add(25);
-        tree.add(66);
-        tree.add(15);
-        tree.add(5);
-        tree.add(20);
-        tree.add(45);
-        tree.add(30);
-        tree.add(32);
-        tree.add(55);
-
-        tree.display();
-
-        System.out.println("Find 60: " + tree.find(60));
-        System.out.println("Find 45: " + tree.find(45));
-        System.out.println("Find 32: " + tree.find(32));
-        System.out.println("Find 666: " + tree.find(666));
-
-        tree.remove(15);
-        tree.display();
-
-
-        TreeImpl.isBalanced(((TreeImpl<Integer>) tree).getRootNode());
+        System.out.println(statistic(treeList));
+       // System.out.println(averageStatistics() + " средняя статистика за 100 итераций");
 
 
     }
@@ -63,18 +32,36 @@ public class Main6 {
         }
     }
 
-   /* public static float statistic (ArrayList<TreeImpl<Integer>> treeList){
-        int countBalance = 0;
-        int countUnbalance = 0;
-        for (int i = 0; i < treeList.size() ; i++) {
-            if (treeList.get(i).isBalanced()){
-                //System.out.println("balanced");
+    public static float statistic (ArrayList<TreeImpl<Integer>> treeList){
+        float countBalance = 0;
+        float countUnbalance = 0;
+        for (int i = 0; i < treeList.size(); i++) {
+            if(TreeImpl.isBalanced(( treeList.get(i)).getRootNode())){
                 countBalance++;
+            } else {
+            countUnbalance++;
             }
-                countUnbalance++;
         }
+
         System.out.println(countBalance);
         System.out.println(countUnbalance);
-        return countBalance/(countBalance+countUnbalance);
+        float result = countBalance/(countBalance+countUnbalance);
+        System.out.println(result);
+        return result;
+    }
+/*
+    public static float averageStatistics () {
+        int count = 100;
+        float average =0f;
+        ArrayList<TreeImpl<Integer>> treeList = new ArrayList<>();
+        for (int i = 0; i <= count; i++) {
+            addTreeCycle(treeList, 20, 15);
+            float temp = statistic(treeList) ;
+            average += temp;
+            System.out.println("статистика " + i + " итерации " +  temp);
+        }
+        float result = average/100;
+        return result;
+
     }*/
 }
